9d65cb4098d2da045c6d69ac14542f9bde902b76
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-06-04 13:24:28 -0700

    write Coord.prototype.plus that adds new coord to this.pos; write Coord.prototype.equals to test if new coord is equal to Coord.pos

--- src/coord.js
+++ src/coord.js
@@ -2,6 +2,19 @@ class Coord {
   constructor (pos) {
     this.pos = pos;
   }
+
+  plus (otherCoord) {
+    this.pos[0] += otherCoord[0];
+    this.pos[1] += otherCoord[1];
+  }
+
+  equals (otherCoord) {
+    if (this.pos[0] === otherCoord[0] && this.pos[1] === otherCoord[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Coord;
