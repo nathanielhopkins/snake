65d59f06adc56d1007f1d9d5ba728e625b5c1b40
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-06-07 12:15:34 -0700

    create View.prototype.keyListener that binds onkeydown event to this.$el; on keydown call helper method handleKeyEvent on event.target; write handleKeyEvent, which takes in event.target (the key pressed), finds the keyCode() and calls Snake.prototype.turn in the appropriate direction when arrow keys are pressed

--- src/snake-view.js
+++ src/snake-view.js
@@ -5,4 +5,24 @@ class View {
     this.board = new Board();
     this.$el = $el;
   }
+
+  keyListener() {
+    this.$el.on("keydown", event => {
+      handleKeyEvent(event.target);
+    })
+  }
+
+  handleKeyEvent(key) {
+    let keyCode = key.keyCode();
+    console.log(keyCode);
+    if(keyCode == 37) {
+      this.board.snake.turn("W");
+    } else if (keyCode === 38) {
+      this.board.snake.turn("N");
+    } else if (keyCode === 39) {
+      this.board.snake.turn("E");
+    } else if (keyCode === 40) {
+      this.board.snake.turn("S")
+    }
+  }
 }
